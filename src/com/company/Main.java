57e771b556275fc81ecadb8af5c482cbda1e62fe
dante <dantefe02@gmail.com>
2020-03-17 20:25:40 -0300

    gitignore

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+//1. Crear la clase Persona que tenga como atributos nombre (String) y DNI (Integer).
+//2. Definir un constructor para la clase Persona que reciba como parámetro el nombre y el DNI.
+//3. En la clase Main, crear dos personas con el mismo número de DNI.
+//4. Comparar utilizando equals si la primera persona creada es igual a la segunda. ¿Cuál es su resultado?
+//5. Sobreescribir el equals de la clase Persona, para que solo compare el numero de DNI.
+//6. Realizar nuevamente la comparación utilizando el equals. ¿Cuál es su resultado? ¿Qué cambió? ¿Por qué?
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
